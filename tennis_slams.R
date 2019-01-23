826651b0ef3f8960f2ff61e042b7e78f4c2acd51
turneralex <alexanderturner1991@gmail.com>
2019-01-23 21:47:57 +1100

    Finalised most of data pre-processing.

--- tennis_slams.R
+++ tennis_slams.R
@@ -25,7 +25,9 @@ slam_winners <- slam_winners %>%
 
 slam_winners %>% 
     select(-1) %>% 
-    map(~ which(. == "Roger Federer"))
+    map(~ which(. == "Roger Federer")) %>% 
+    unlist() %>% 
+    min()
 
 slam_winners_sub <- slam_winners %>% 
     slice(129:nrow(.))
@@ -47,28 +49,40 @@ all_winners <- slam_winners_sub %>%
 
 all_winners
 
-big_four <- all_winners[c(1, 3, 4, 7)]
+big_four <- all_winners[c(1, 3, 4, 7)] %>% 
+    sort()
 
 slam_winners_sub %>%
     select(-1) %>% 
     map(table) %>% 
     map(~ as_tibble(., .name_repair = make.names) %>% 
-            arrange(desc(n)))
+            arrange(desc(n))) 
 
+slam_winners_sub <- slam_winners_sub %>% 
+    gather(slam, player, -year) %>% 
+    mutate(year = year %>% factor(),
+           slam = slam %>% factor(labels = c("Australian Open", "French Open", "US Open", "Wimbledon")) %>% 
+               fct_relevel("Australian Open", "French Open", "Wimbledon"),
+           player = if_else(player %in% big_four, player, "Other Player") %>% 
+               factor(labels = c(big_four[1:2], "Other Player", big_four[3:4])) %>% 
+               fct_relevel(big_four, "Other Player"))
+    
+    
 test <- slam_winners_sub %>% 
     gather(slam, player, -year) %>% 
     mutate(indicator = 1) %>% 
     spread(player, indicator, fill = 0) %>% 
     mutate_if(is.double, cumsum) %>% 
-    gather(player, total, -year, -slam) %>% 
-    arrange(year, slam) %>% 
+    mutate(other_player = select(., -year, -slam, -big_four) %>% 
+               pmap_dbl(lift_vd(sum))) %>% 
+    select(year, slam, big_four, other_player) %>% 
+    gather(player, cumulative_total, -year, -slam) %>% 
     mutate(year = year %>% factor(),
            slam = slam %>% factor(labels = c("Australian Open", "French Open", "US Open", "Wimbledon")) %>% 
-               fct_relevel("Australian Open", "French Open", "Wimbledon"))
+               fct_relevel("Australian Open", "French Open", "Wimbledon"),
+           player = player %>% factor(labels = c(big_four[1:2], "Other Player", big_four[3:4])) %>% 
+               fct_relevel(big_four, "Other Player"))
 
-
-
-
-
-
-
+test %>% 
+    ggplot(aes(year, cumulative_total, colour = player, group = player)) +
+    geom_line(size = 2, alpha = 0.8)

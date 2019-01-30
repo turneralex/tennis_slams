32e8b94b72beb02dc19822672e78a632598f5e94
turneralex <alexanderturner1991@gmail.com>
2019-01-30 21:17:01 +1100

    Refector most of data-preprocessing.

--- tennis_slams.R
+++ tennis_slams.R
@@ -1,48 +1,54 @@
 library(tidyverse)
-library(gganimate)
-library(lubridate)
+# library(gganimate)
+# library(lubridate)
 library(rvest)
 
+# wikipedia url
 slams_url <- "https://en.wikipedia.org/wiki/List_of_Grand_Slam_men%27s_singles_champions"
 
+# import data
 slam_winners <- slams_url %>% 
     read_html() %>% 
     html_nodes(xpath = '//*[@id="mw-content-text"]/div/table[1]') %>% 
-    html_table() 
-
-slam_winners <- slam_winners[[1]] 
-
-slam_winners <- slam_winners %>% 
-    as_tibble()
+    html_table(fill = T) %>% 
+    magrittr::extract2(1) %>% # extract data frame from list     
+    as_tibble() # convert to tibble for better print method
 
 slam_winners
 
+# change column names to snake case
 colnames(slam_winners) <- colnames(slam_winners) %>% 
     tolower() %>% 
     str_replace_all(" ", "_")
 
+# remove all characters that are non-alphabetic excluding whitespace
 slam_winners <- slam_winners %>%
     mutate_if(is.character, ~ str_remove_all(., "[^[:alpha:] ]") %>% str_trim()) 
 
+# check which row has roger federer's 1st grand slam win
 slam_winners %>% 
-    select(-1) %>% 
-    map(~ which(. == "Roger Federer")) %>% 
-    unlist() %>% 
+    select(-year) %>% 
+    map(~ str_which(., "Roger Federer")) %>% 
+    flatten_dbl() %>% 
     min()
 
+# create subset dataframe for period of interest
 slam_winners_sub <- slam_winners %>% 
-    slice(129:nrow(.))
+    slice(128:(nrow(.) - 1)) # remove 2019 data also
 
 slam_winners_sub
 
+# check the list of winners from 2003 - 2018
 slam_winners_sub %>% 
     select(-1) %>% 
     unlist() %>% 
     unique()
 
-slam_winners_sub <- slam_winners_sub %>%
-    mutate(australian_open = if_else(australian_open == "SRB Novak Djokovic", "Novak Djokovic", australian_open)) 
+# fix name errors
+slam_winners_sub <- slam_winners_sub %>% 
+    mutate_if(~ str_detect(., "SRB|SUI") %>% any(), ~ str_remove(., "SRB |SUI "))
 
+# create vector all winners 2003 - 2018
 all_winners <- slam_winners_sub %>% 
     select(-1) %>% 
     unlist() %>% 
@@ -50,25 +56,31 @@ all_winners <- slam_winners_sub %>%
 
 all_winners
 
-big_four <- all_winners[c(1, 3, 4, 7)] %>% 
-    sort()
+# create vector for the 'big four'
+big_four <- all_winners[c(2, 5, 4, 9)]
 
+# create summaries of winner totals for each grand slam
 slam_winners_sub %>%
     select(-1) %>% 
     map(table) %>% 
     map(~ as_tibble(., .name_repair = make.names) %>% 
-            arrange(desc(n))) 
+            mutate(., X = if_else(X %in% big_four, X, "Other Player")) %>% 
+            group_by(X) %>% 
+            summarise(total_slams = sum(n)) %>% 
+            arrange(desc(total_slams))) 
 
+
+# convert data to long / tidy format
 slam_winners_sub_long <- slam_winners_sub %>% 
     gather(slam, player, -year) %>% 
     mutate(year = year %>% factor(),
            slam = slam %>% factor(labels = c("Australian Open", "French Open", "US Open", "Wimbledon")) %>% 
                fct_relevel("Australian Open", "French Open", "Wimbledon"),
            player = if_else(player %in% big_four, player, "Other Player") %>% 
-               factor(labels = c(big_four[1:2], "Other Player", big_four[3:4])) %>% 
                fct_relevel(big_four, "Other Player"))
-    
-    
+
+slam_winners_sub_long 
+
 cumulative_slams <- slam_winners_sub %>% 
     gather(slam, player, -year) %>% 
     mutate(indicator = 1) %>% 
@@ -81,13 +93,12 @@ cumulative_slams <- slam_winners_sub %>%
     mutate(year = year %>% factor(),
            slam = slam %>% factor(labels = c("Australian Open", "French Open", "US Open", "Wimbledon")) %>% 
                fct_relevel("Australian Open", "French Open", "Wimbledon"),
-           player = player %>% factor(labels = c(big_four[1:2], "Other Player", big_four[3:4])) %>% 
+           player = player %>% factor(labels = c(big_four[4:3], "Other Player", big_four[2:1])) %>% 
                fct_relevel(big_four, "Other Player"))
 
 cumulative_slams %>% 
     ggplot(aes(year, cumulative_total, colour = player, group = player)) +
-    geom_line(size = 2, alpha = 0.8) +
-    transition_reveal(as.numeric(year))
+    geom_line(size = 2, alpha = 0.8) 
 
 slam_winners_sub_long %>% 
     mutate(player = if_else(player %in% big_four, "Big Four", "Other Player") %>% 
@@ -102,3 +113,7 @@ slam_winners_sub_long %>%
     ggplot(aes(player, slam, fill = total_slams)) +
     geom_raster()
     
+# cumulative_slams %>% 
+#     ggplot(aes(year, cumulative_total, colour = player, group = player)) +
+#     geom_line(size = 2, alpha = 0.8) +
+#     transition_reveal(as.numeric(year))

ab56f77400919883228e46f44144b0f6f1e9808c
turneralex <alexanderturner1991@gmail.com>
2019-01-31 21:23:36 +1100

    Finalise data preprocessing & plots script.

--- tennis_slams.R
+++ tennis_slams.R
@@ -27,7 +27,7 @@ slam_winners <- slam_winners %>%
 slam_winners %>% 
     select(-year) %>% 
     map(~ str_which(., "Roger Federer")) %>% 
-    flatten_dbl() %>% 
+    flatten_int() %>% 
     min()
 
 # create subset data frame for period of interest
@@ -38,8 +38,8 @@ slam_winners_sub
 
 # check the list of winners from 2003 - 2018
 slam_winners_sub %>% 
-    select(-1) %>% 
-    unlist() %>% 
+    select(-year) %>% 
+    flatten_chr() %>% 
     unique()
 
 # fix name errors
@@ -48,7 +48,7 @@ slam_winners_sub <- slam_winners_sub %>%
 
 # create vector all winners 2003 - 2018
 all_winners <- slam_winners_sub %>% 
-    select(-1) %>% 
+    select(-year) %>% 
     unlist() %>% 
     unique()
 
@@ -57,16 +57,22 @@ all_winners
 # create vector for the 'big four'
 big_four <- all_winners[c(2, 5, 4, 9)]
 
-# create summaries of winner totals for each grand slam
+# create summary plots of totals for each player by slam
 slam_winners_sub %>%
-    select(-1) %>% 
-    map(table) %>% 
-    map(~ as_tibble(., .name_repair = make.names) %>% 
-            mutate(., X = if_else(X %in% big_four, X, "Other Player")) %>% 
-            group_by(X) %>% 
+    select(-year) %>% 
+    map(~ table(.) %>% 
+            as_tibble(.name_repair = make.names) %>% 
+            mutate(player = if_else(. %in% big_four, ., "Other Player")) %>% 
+            group_by(player) %>% 
             summarise(total_slams = sum(n)) %>% 
-            arrange(desc(total_slams))) 
-
+            mutate(x = if_else(player %in% big_four, "Big Four", "Other Player")) %>% 
+            ggplot(aes(reorder(player, -total_slams), total_slams, fill = x)) + 
+            geom_col() +
+            scale_fill_brewer(palette = "Set2") +
+            labs(title = "Big 4 vs The Rest",
+                 x = "Player",
+                 y = "Total Wins") + 
+            guides(fill = F)) 
 
 # convert data to long / tidy format
 slam_winners_sub_long <- slam_winners_sub %>% 
@@ -77,42 +83,32 @@ slam_winners_sub_long <- slam_winners_sub %>%
            player = if_else(player %in% big_four, player, "Other Player") %>% 
                fct_relevel(big_four, "Other Player"))
 
-slam_winners_sub_long 
-
 # prepare cumulative slams by player data frame
-cumulative_slams <- slam_winners_sub %>% 
-    gather(slam, player, -year) %>% 
+cumulative_slams <- slam_winners_sub_long %>% 
     mutate(indicator = 1) %>% 
     spread(player, indicator, fill = 0) %>% 
     mutate_if(is.double, cumsum) %>% 
-    mutate(other_player = select(., -year, -slam, -big_four) %>% 
-               pmap_dbl(lift_vd(sum))) %>% 
-    select(year, slam, big_four, other_player) %>% 
     gather(player, cumulative_total, -year, -slam) %>% 
-    mutate(year = year %>% factor(),
-           slam = slam %>% factor(labels = c("Australian Open", "French Open", "US Open", "Wimbledon")) %>% 
-               fct_relevel("Australian Open", "French Open", "Wimbledon"),
-           player = player %>% factor(labels = c(big_four[4:3], "Other Player", big_four[2:1])) %>% 
-               fct_relevel(big_four, "Other Player"))
+    mutate(player = player %>% fct_relevel(big_four, "Other Player"))
 
+# cumulative slams plot
 cumulative_slams %>% 
     ggplot(aes(year, cumulative_total, colour = player, group = player)) +
-    geom_line(size = 2, alpha = 0.8) 
+    geom_line(size = 2, alpha = 0.8) +
+    scale_colour_brewer(palette = "Set1") +
+    labs(title = "Cumulative Total Grand Slams by Player",
+         x = "Year",
+         y = "Cumulative Total",
+         colour = "Player")
 
+# tile plot
 slam_winners_sub_long %>% 
     mutate(player = if_else(player %in% big_four, "Big Four", "Other Player") %>% 
                factor()) %>% 
-    ggplot(aes(year, slam, fill = player)) +
-    geom_tile(colour = "grey")
-
-slam_winners_sub_long %>% 
-    mutate(indicator = 1) %>% 
-    group_by(player, slam) %>% 
-    summarise(total_slams = indicator %>% sum()) %>% 
-    ggplot(aes(player, slam, fill = total_slams)) +
-    geom_raster()
-    
-# cumulative_slams %>% 
-#     ggplot(aes(year, cumulative_total, colour = player, group = player)) +
-#     geom_line(size = 2, alpha = 0.8) +
-#     transition_reveal(as.numeric(year))
+    ggplot(aes(year, fct_rev(slam), fill = player)) +
+    geom_tile(colour = "black") +
+    scale_fill_brewer(palette = "Set2") +
+    labs(title = "Cumulative Total Grand Slams by Player",
+         x = "Year",
+         y = "Grand Slam",
+         fill = "Winner")
